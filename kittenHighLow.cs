550335bbed88fc7f3339acd9705df1a18f0d1f1f
MuttKitten <139673834+Muttiskitten@users.noreply.github.com>
2026-04-21 16:33:32 -0400

    f"string" update
    Made updates to the output.
    Example:
    Console.WriteLine("welcome " + userName)
    to
    Console.WriteLine($"Welcome {userName}")
    
    Every string that has a veriable is now an f"string"

--- kittenHighLow.cs
+++ kittenHighLow.cs
@@ -12,7 +12,7 @@ namespace HighLowGame
       Console.WriteLine("Can i get your cat's name?");
       string userName = Console.ReadLine();
 
-      Console.WriteLine("Welcome " + userName);
+      Console.WriteLine($"Welcome {userName}");
 
       Random random = new Random();
       int score = 0;
@@ -23,31 +23,31 @@ namespace HighLowGame
       {
 
     int firstNumber = random.Next(1, 100);
-    Console.WriteLine("First number is: " + firstNumber);
+    Console.WriteLine($"First number is: {firstNumber}");
 
     Console.WriteLine("Is the next number Higher or Lower? (h/l) ");
     string guess = Console.ReadLine().ToLower();
 
     int secondNumber = random.Next(1, 100);
-    Console.WriteLine("Second number is: " + secondNumber);
+    Console.WriteLine($"Second number is: {secondNumber}");
 
     if (secondNumber == firstNumber)
     {
-        Console.WriteLine("Its a Match " + userName);
+        Console.WriteLine($"Its a Match {userName}");
     }
     else if (guess == "h" && secondNumber > firstNumber)
     {
-        Console.WriteLine("Correct! "+ userName);
+        Console.WriteLine($"Correct! {userName}");
         score++;
     }
     else if (guess == "l" && secondNumber < firstNumber)
     {
-        Console.WriteLine("Correct! " + userName);
+        Console.WriteLine($"Correct! {userName}");
         score++;
     }
     else if (guess == "h" || guess == "l")
     {
-        Console.WriteLine("Wrong! " + userName);
+        Console.WriteLine($"Wrong! {userName}");
         score--;
     }
     else
@@ -55,15 +55,15 @@ namespace HighLowGame
         Console.WriteLine("Sorry you must use (h or l)!");
     }
     
-    Console.WriteLine("Score: " + score);    
+    Console.WriteLine($"Score: {score}");    
       }
       if (score == winning)
       {
-          Console.WriteLine("Winner: " + userName);
+          Console.WriteLine($"Winner: {userName}");
       }
       else if (score == -5)
       {
-          Console.WriteLine("Dang, you Lost: " + userName);
+          Console.WriteLine($"Dang, you Lost: {userName}");
       }
   }
     }
